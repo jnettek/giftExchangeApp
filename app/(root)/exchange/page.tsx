391448c516bbd036681588df1ea822cbee2678b6
jnettek <jnettekpelletier@live.com>
2024-02-19 15:39:03 -0500

    exchange page

--- app/(root)/exchange/page.tsx
+++ app/(root)/exchange/page.tsx
@@ -26,6 +26,7 @@ const exchangePage = () => {
       eventDate: new Date(),
       budget: "",
       invitationMessage: "",
+      participants: [],
     },
   })
 
--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,8 +1,15 @@
 import * as z from "zod";
 
+const participantSchema = z.object({
+    name: z.string().min(1, 'Participant name is required'),
+    email: z.string().email('Invalid email address'),
+  });
+
 export const userSchema = z.object({
     eventName: z.string(),
     eventDate: z.date(),
-    budget: z.string(), // Assign a valid Zod type to the budget property
+    budget: z.string().regex(/^\d+$/, "Budget must be a number"), // Assign a valid Zod type to the budget property
     invitationMessage: z.string().min(2).max(244),
+    participants: z.array(participantSchema).min(1),
+
 });
