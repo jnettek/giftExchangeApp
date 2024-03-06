d3cc40bdf280bdfdec7c624b7304e16871d361a1
jnettek <jnettekpelletier@live.com>
2024-03-05 21:38:52 -0500

    form modified

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,1 +1,9 @@
 "use server"
+import { User } from "../models/user.model";
+import { connectToDB } from "../validations/mongoose";
+
+export async function createUser(): Promise<void> {
+  await connectToDB();
+
+  await User.findOneAndUpdate()
+}
---
+++ lib/validations/mongoose.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+let isConnected: boolean = false; // variable to check if the connection is already established
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if(!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
+    if(isConnected) return console.log('Already connected to DB');
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL) 
+        isConnected = true;
+        console.log('Connected to DB');
+    } catch (error) {
+        console.log('Error connecting to DB', error);
+    }
+}
